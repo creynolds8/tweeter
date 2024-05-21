9939ccc74cc24b592a31d3e40e6628caa0bdc7ee
creynolds8 <cameron8.cr@gmail.com>
2024-05-21 15:43:17 -0700

    implement color change when over limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,14 @@
 $(() => {
-  console.log('loaded!');
+  console.log("loaded!");
 
-  $('form textarea').on('input', () => {
-    let tweetLength = $('#tweet-text').val().length
-    let counter = document.getElementById('counter')
+  $("form textarea").on("input", () => {
+    let tweetLength = $("#tweet-text").val().length;
+    let counter = document.getElementById("counter");
     counter.innerHTML = 140 - tweetLength;
-  })
-
-})
+    if (counter.innerHTML <= 0) {
+      document.getElementById("counter").style.color = "red";
+    } else {
+      document.getElementById("counter").style.color = "inherit";
+    }
+  });
+});
