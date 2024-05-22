2b2d7fa8938a77253e70168cddba19f966c92ab5
creynolds8 <cameron8.cr@gmail.com>
2024-05-22 13:01:56 -0700

    auto format file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,37 +30,37 @@ const createTweetElement = function (tweet) {
   return newTweet;
 };
 
-const renderTweets = function(tweets) {
+const renderTweets = function (tweets) {
   for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet)
-    $('#tweets-container').prepend($tweet)
+    const $tweet = createTweetElement(tweet);
+    $("#tweets-container").prepend($tweet);
   }
-}
+};
 
 const tweetData = [
   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
     },
-    "created_at": 1461116232227
+    created_at: 1461116232227,
   },
   {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
     },
-    "created_at": 1461113959088
-  }
-]
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
 $(() => {
   renderTweets(tweetData);
