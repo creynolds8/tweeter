ec8ae3c9d3f3b234273ec6625daa3e154e1d308e
creynolds8 <cameron8.cr@gmail.com>
2024-05-22 12:41:26 -0700

    add render tweets functions, works with in file data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,46 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(() => {
-  const createTweetElement = function (tweet) {
-    const newTweet = $(`
-    <article class="tweet">
-    <header>
-      <span><img src=${tweet.user.avatar}/> and ${tweet.user.name}</span>
-      ${tweet.user.handle}
-    </header>
-      <p>
-       ${tweet.content.text}
-      </p>
-    <footer>
-      ${tweet.created_at}
-      <div class="icons">
-        <div>
-          <i class="fa-solid fa-heart"></i>
-        </div>
-        <div>
-          <i class="fa-solid fa-retweet"></i>
-        </div>
-        <div>
-          <i class="fa-solid fa-flag"></i>
-        </div>
-      </div>`);
-    return newTweet;
-  };
-  
-  const tweetData = {
+const createTweetElement = function (tweet) {
+  const newTweet = $(`
+  <article class="tweet">
+  <header>
+  <span><img src=${tweet.user.avatars}/>${tweet.user.name}</span>
+  ${tweet.user.handle}
+  </header>
+  <p>
+  ${tweet.content.text}
+  </p>
+  <footer>
+  ${tweet.created_at}
+  <div class="icons">
+  <div>
+  <i class="fa-solid fa-heart"></i>
+  </div>
+  <div>
+  <i class="fa-solid fa-retweet"></i>
+  </div>
+  <div>
+  <i class="fa-solid fa-flag"></i>
+  </div>
+  </div>`);
+  return newTweet;
+};
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet)
+    $('#tweets-container').prepend($tweet)
+  }
+}
+
+const tweetData = [
+  {
     "user": {
       "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   }
-  
-  const tweet = createTweetElement(tweetData)
-  console.log(tweet);
-  $('#tweets-container').append(tweet);
+]
+
+$(() => {
+  renderTweets(tweetData);
 });
