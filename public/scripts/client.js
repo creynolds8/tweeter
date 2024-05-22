856b2bba5e72f4413e76b134c9842be5ca9387a1
creynolds8 <cameron8.cr@gmail.com>
2024-05-22 09:15:46 -0700

    add create tweet function, first draft test to follow

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,34 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(() => {
+
+  const createTweetElement = function(tweet) {
+    const newTweet = $(`
+    <article class="tweet">
+    <header>
+      <span><img src=${tweet.user.avatar}/> and ${tweet.user.name}</span>
+      ${tweet.user.handle}
+    </header>
+      <p>
+       ${tweet.content.text}
+      </p>
+    <footer>
+      ${tweet.created_at}
+      <div class="icons">
+        <div>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+        <div>
+          <i class="fa-solid fa-retweet"></i>
+        </div>
+        <div>
+          <i class="fa-solid fa-flag"></i>
+        </div>
+      </div>`)
+  }
+
+
+
+
+})
