e87bdee7868fd687a88380c8fd165439f0b40323
creynolds8 <cameron8.cr@gmail.com>
2024-05-22 09:42:07 -0700

    add test code, it works!

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,4 +30,20 @@ $(() => {
       </div>`);
     return newTweet;
   };
+  
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+  
+  const tweet = createTweetElement(tweetData)
+  console.log(tweet);
+  $('#tweets-container').append(tweet);
 });
