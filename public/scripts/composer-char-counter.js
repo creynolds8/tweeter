6326acc326ec493e7e4224a25fb29811eb7c72b5
creynolds8 <cameron8.cr@gmail.com>
2024-05-23 19:38:36 -0700

    final format and lint check

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,9 +4,9 @@ $(() => {
     let counter = $(this).siblings("#button-counter").find("#counter");
     counter.html(140 - tweetLength);
     if (counter.html() <= 0) {
-      counter.css('color', 'red');
+      counter.css("color", "red");
     } else {
-      counter.css('color', 'inherit');
+      counter.css("color", "inherit");
     }
   });
 });
